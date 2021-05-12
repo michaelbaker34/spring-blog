8eb831e1b5766b28cfd112c1bc0e346f647ff7aa
Michael Baker <michael.a.baker34@gmail.com>
2021-05-11 22:08:35 -0500

    import userdetailsloader in security config

--- src/main/java/com/codeup/springblog/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/springblog/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.codeup.springblog.config;
 
+import com.codeup.springblog.services.UserDetailsLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
