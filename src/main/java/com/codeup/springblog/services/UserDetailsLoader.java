0a54bd430e595ca9570fe50112a8583e8d0235f3
Michael Baker <michael.a.baker34@gmail.com>
2021-05-11 22:06:57 -0500

    rename findbyusername method

--- src/main/java/com/codeup/springblog/services/UserDetailsLoader.java
+++ src/main/java/com/codeup/springblog/services/UserDetailsLoader.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.services;
 
 import com.codeup.springblog.models.User;
+import com.codeup.springblog.models.UserWithRoles;
 import com.codeup.springblog.repositories.UserRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +16,7 @@ public class UserDetailsLoader implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userDao.findbyUsername(username);
+        User user = userDao.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException(username + " not found.");
         }
